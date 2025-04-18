4060722e7eefaf6fcbf909dbb2c79a157a2691aa
yanis <yanisabdulmadjid@gmail.com>
2025-04-18 09:08:45 +0200

    Modification de la classe etudiant et ajout de methode

--- Etudiant.java
+++ Etudiant.java
@@ -1,32 +1,43 @@
 class Etudiant {
+    private String matricule;
     private String nom;
-    private String adresse;
     private int dateDeNaissance;
-    private int[] notes;
+    private double[] notes;
 
-    public Etudiant(String nom, String adresse, int dateDeNaissance, int[] notes) {
+    public Etudiant(String matricule, String nom, int dateDeNaissance, double[] notes) {
+        this.matricule = matricule;
         this.nom = nom;
-        this.adresse = adresse;
         this.dateDeNaissance = dateDeNaissance;
         this.notes = notes;
     }
 
     public void afficher() {
+        System.out.println("Matricule: " + matricule);
         System.out.println("Nom: " + nom);
-        System.out.println("Adresse: " + adresse);
         System.out.println("Date de naissance: " + dateDeNaissance);
         for (int i = 0; i < notes.length; i++) {
             System.out.println("Note " + (i + 1) + ": " + notes[i]);
         }
+        System.out.println("Moyenne: " + calculerMoyenne());
     }
 
     public double calculerMoyenne() {
         double somme = 0;
-        for (int note : notes) {
+        for (double note : notes) {
             somme += note;
         }
         return somme / notes.length;
     }
 
-   
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double[] getNotes() {
+        return notes;
+    }
 }
